d53d3bd04365081ac366621e456f47fed932475d
Andrea Velazco <avelazcoy@ulasalle.edu.pe>
2024-12-09 20:47:16 -0500

    update puntajes

--- Proyecto_Final-main/app/src/main/java/com/example/preguntados/activities/PuntajesActivity.kt
+++ Proyecto_Final-main/app/src/main/java/com/example/preguntados/activities/PuntajesActivity.kt
@@ -1,14 +1,18 @@
 package com.example.preguntados.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.preguntados.R
+import kotlinx.android.synthetic.main.activity_puntajes.*
+
 
 class PuntajesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_puntajes)
-
+        btnBack.setOnClickListener {
+            finish()
     }
 }
